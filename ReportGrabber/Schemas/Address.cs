9c0ef4cdde8911bd4c6f0ef1b219aa38c1971ded
Ivan Litskevich <sxkote@gmail.com>
2015-06-25 12:05:25 +0300

    init commit

---
+++ ReportGrabber/Schemas/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGrabber.Schemas
+{
+    public struct Address
+    {
+        private string _uri;
+
+        public string Uri
+        { get { return _uri; } }
+
+        public Address(string uri)
+        { _uri = uri; }
+
+        static public implicit operator string (Address address)
+        { return address.Uri; }
+
+        static public implicit operator Address(string address)
+        { return new Address(address); }
+    }
+}
---
+++ ReportGrabber/Schemas/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGrabber.Schemas
+{
+    public struct Field
+    {
+        private Address _address;
+        private string _name;
+
+        public Field(string name, string address)
+        {
+            _name = name;
+            _address = address;
+        }
+
+        public Field(string name, Address address)
+        {
+            _name = name;
+            _address = address;
+        }
+    }
+}
